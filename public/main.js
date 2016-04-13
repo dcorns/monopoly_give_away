3fe86cab3c1ba4a611fd64915ff44f1ec4635bde
Dale <drcorns@hotmail.com>
2016-04-12 22:41:44 -0700

    Improve ux

--- public/main.js
+++ public/main.js
@@ -382,11 +382,169 @@ function setWinningTickets(ary){
   }
 }
 
-function ticketInput(){
-  var ticket = document.getElementById('ticket').value;
-  console.dir(prizeData);
+function ticketInput(value){
+  var ticket = value || document.getElementById('ticket').value.toUpperCase();
+  var i = 0, len = prizeData.length;
+  for (i; i < len; i++){
+    for (var prop in prizeData[i]){
+      if(prop === 'tickets'){
+        for(var prop1 in prizeData[i][prop]) {
+          if (prop1 === 'winner') {
+            if (prizeData[i][prop][prop1] === ticket && ticket !== '') {
+              console.log('It\'s a Winner!');
+              prizeData[i][prop][prop1] = "WINNER!";
+              youWin(prizeData[i].viewId);
+            }
+          }
+          if(prop1 === 'partList'){
+            for(var ii=0; ii < prizeData[i][prop].required; ii += 2){
+              if(prizeData[i][prop][prop1][ii] === ticket){
+                prizeData[i][prop][prop1][ii + 1]++;
+                updatePrize(prizeData[i]);
+                addTicketMessage(prizeData[i].viewId, ticket, prizeData[i][prop][prop1][ii + 1]);
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+function youWin(viewId){
+  var elId = 'w' + viewId.substr(1);
+  var el = document.getElementById(elId);
+  el.textContent = 'WINNER!';
+  el.classList.add('winner');
+  el.classList.remove('winnerTxt');
+}
+
+function addTicketMessage(viewId, ticket, value){
+  var elId = 'w' + viewId.substr(1);
+  var el = document.getElementById(elId);
+  el.textContent = ticket + ' = ' + value;
+}
+
+function enlargeCard(viewId){
+  var idx = 0, c = 0;
+  for(c;c<prizeData.length;c++){
+    if(prizeData[c].viewId === viewId){
+      idx = c;
+      break;
+    }
+  }
+  currentPrize = setCurrentPrize(prizeData[idx]);
+  currentIndex = idx;
+  var x = e.target.x.baseVal.value;
+  var y = e.target.y.baseVal.value;
+  prizes.setAttribute('viewBox', (x - 1).toString()+' '+ (y + 4).toString() + ' ' + '112 ' + '75');
+  goBack.setAttribute('cx', (x + 105).toString());
+  goBack.setAttribute('cy', (y + 5).toString());
+  goBack.setAttribute('data-PrizeId', prizeId);
+  winnerTxt.setAttribute('x', (x + 55).toString());
+  winnerTxt.setAttribute('y', (y + 24).toString());
+  winnerTxt.textContent = 'Winning Ticket: ' + prizeData[idx].tickets.winner;
+  add0.setAttribute('cx', (x + addC1Xoffset).toString());
+  add0.setAttribute('cy', (y + 32).toString());
+  addTxt0.setAttribute('x', (x + addC1Xoffset).toString());
+  addTxt0.setAttribute('y', (y + 34).toString());
+  addTxt0.textContent = prizeData[idx].tickets.partList[1];
+  part1.setAttribute('x', (x + partC1Xoffset).toString());
+  part1.setAttribute('y', (y + 35).toString());
+  part1.textContent = prizeData[idx].tickets.partList[0];
+  minus0.setAttribute('cx', (x + minusC1Xoffset).toString());
+  minus0.setAttribute('cy', (y + 32).toString());
+
+  add2.setAttribute('cx', (x + addC2Xoffset).toString());
+  add2.setAttribute('cy', (y + 32).toString());
+  addTxt2.setAttribute('x', (x + addC2Xoffset).toString());
+  addTxt2.setAttribute('y', (y + 34).toString());
+  addTxt2.textContent = prizeData[idx].tickets.partList[3];
+  part2.setAttribute('x', (x + partC2Xoffset).toString());
+  part2.setAttribute('y', (y + 35).toString());
+  part2.textContent = prizeData[idx].tickets.partList[2];
+  minus2.setAttribute('cx', (x + minusC2Xoffset).toString());
+  minus2.setAttribute('cy', (y + 32).toString());
+
+  add4.setAttribute('cx', (x + addC1Xoffset).toString());
+  add4.setAttribute('cy', (y + 45).toString());
+  addTxt4.setAttribute('x', (x + addC1Xoffset).toString());
+  addTxt4.setAttribute('y', (y + 47).toString());
+  addTxt4.textContent = prizeData[idx].tickets.partList[5];
+  part3.setAttribute('x', (x + partC1Xoffset).toString());
+  part3.setAttribute('y', (y + 48).toString());
+  part3.textContent = prizeData[idx].tickets.partList[4];
+  minus4.setAttribute('cx', (x + minusC1Xoffset).toString());
+  minus4.setAttribute('cy', (y + 45).toString());
+
+  add6.setAttribute('cx', (x + addC2Xoffset).toString());
+  add6.setAttribute('cy', (y + 45).toString());
+  addTxt6.setAttribute('x', (x + addC2Xoffset).toString());
+  addTxt6.setAttribute('y', (y + 47).toString());
+  addTxt6.textContent = prizeData[idx].tickets.partList[7];
+  part4.setAttribute('x', (x + partC2Xoffset).toString());
+  part4.setAttribute('y', (y + 48).toString());
+  part4.textContent = prizeData[idx].tickets.partList[6];
+  minus6.setAttribute('cx', (x + minusC2Xoffset).toString());
+  minus6.setAttribute('cy', (y + 45).toString());
+
+  if (prizeData[idx].tickets.partList[8]) {
+    add8.setAttribute('cx', (x + addC1Xoffset).toString());
+    add8.setAttribute('cy', (y + 58).toString());
+    addTxt8.setAttribute('x', (x + addC1Xoffset).toString());
+    addTxt8.setAttribute('y', (y + 60).toString());
+    addTxt8.textContent = prizeData[idx].tickets.partList[9];
+    part5.setAttribute('x', (x + partC1Xoffset).toString());
+    part5.setAttribute('y', (y + 61).toString());
+    part5.textContent = prizeData[idx].tickets.partList[8];
+    minus8.setAttribute('cx', (x + minusC1Xoffset).toString());
+    minus8.setAttribute('cy', (y + 58).toString());
+    if(prizeData[idx].tickets.partList[10]){
+      add10.setAttribute('cx', (x + addC2Xoffset).toString());
+      add10.setAttribute('cy', (y + 58).toString());
+      addTxt10.setAttribute('x', (x + addC2Xoffset).toString());
+      addTxt10.setAttribute('y', (y + 60).toString());
+      addTxt10.textContent = prizeData[idx].tickets.partList[11];
+      part6.setAttribute('x', (x + partC2Xoffset).toString());
+      part6.setAttribute('y', (y + 61).toString());
+      part6.textContent = prizeData[idx].tickets.partList[10];
+      minus10.setAttribute('cx', (x + minusC2Xoffset).toString());
+      minus10.setAttribute('cy', (y + 58).toString());
+      if(prizeData[idx].tickets.partList[12]){
+        add12.setAttribute('cx', (x + addC1Xoffset).toString());
+        add12.setAttribute('cy', (y + 71).toString());
+        addTxt12.setAttribute('x', (x + addC1Xoffset).toString());
+        addTxt12.setAttribute('y', (y + 73).toString());
+        addTxt12.textContent = prizeData[idx].tickets.partList[13];
+        part7.setAttribute('x', (x + partC1Xoffset).toString());
+        part7.setAttribute('y', (y + 74).toString());
+        part7.textContent = prizeData[idx].tickets.partList[12];
+        minus12.setAttribute('cx', (x + minusC1Xoffset).toString());
+        minus12.setAttribute('cy', (y + 71).toString());
+        if(prizeData[idx].tickets.partList[14]){
+          add14.setAttribute('cx', (x + addC2Xoffset).toString());
+          add14.setAttribute('cy', (y + 71).toString());
+          addTxt14.setAttribute('x', (x + addC2Xoffset).toString());
+          addTxt14.setAttribute('y', (y + 73).toString());
+          addTxt14.textContent = prizeData[idx].tickets.partList[15];
+          part8.setAttribute('x', (x + partC2Xoffset).toString());
+          part8.setAttribute('y', (y + 74).toString());
+          part8.textContent = prizeData[idx].tickets.partList[14];
+          minus14.setAttribute('cx', (x + minusC2Xoffset).toString());
+          minus14.setAttribute('cy', (y + 71).toString());
+        }
+      }
+    }
+  }
 }
 
 document.getElementById('btnEnter').addEventListener('click', function(){
   ticketInput();
+});
+document.getElementById('ticket').addEventListener('keyup', function(e){
+  if(e.keyCode === 13){
+    ticketInput(e.target.value.toUpperCase());
+    e.target.value = '';
+  }
 });
