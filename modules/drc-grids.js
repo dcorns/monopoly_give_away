016cf688c7fdd39c2a8dfe5235f084ad213c6767
Dale <drcorns@hotmail.com>
2017-04-05 22:05:37 -0700

    add rowsEqual function to drc-grids

--- modules/drc-grids.js
+++ modules/drc-grids.js
@@ -9,4 +9,14 @@ module.exports = {
     if(!(Array.isArray(row))) throw new TypeError('Input must be an array');
     return row.filter(r => typeof r === 'string');
   },
+  rowsEqual: (r1, r2) => {
+    if(!(Array.isArray(r1)) || !(Array.isArray(r2))) throw new TypeError('Input must be two arrays');
+    if(!(r1.length === r2.length)) return false;
+    if(r1.length < 100) {
+      return  r1.reduce((out, r) => {
+        return out && (r === r2[r1.indexOf(r)]);
+      }, true);
+    }
+    else throw new TypeError('Arrays are too large');
+  },
 };
--- test/drc-grids.spec.js
+++ test/drc-grids.spec.js
@@ -3,6 +3,7 @@ const expect = require ('chai').expect;
 
 const drcGrids = require('../modules/drc-grids');
 describe('drc-grids.js', () => {
+
   describe('getRowStrings', () => {
     it('Input: Array, Output: Array containing only items with string values from input', () => {
       expect(drcGrids.getRowStrings(['f', 108, 'a', 34, 'g', 4, '', 34, 23, 108])).to.deep.equal(['f', 'a', 'g', '']);
@@ -15,6 +16,23 @@ describe('drc-grids.js', () => {
     });
     it('Returns an empty array if the array does not contain strings', () => {
       expect(drcGrids.getRowStrings([108, 34, 4, 34, 23, 108, {}])).to.deep.equal([]);
+    });
+  });
+  describe('rowsEqual', () => {
+    it('Returns true if two arrays are given as input and they are equal', () => {
+      expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello'])).to.be.true;
+      expect(drcGrids.rowsEqual([], [])).to.be.true;
+    });
+    it('Returns false if two arrays inputs are not equal', () => {
+      expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello'].reverse())).to.be.false;
+      expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello world'])).to.be.false;
+      expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello', 'plus one'])).to.be.false;
+    });
+    it('Throws a type error if input is not two arrays', () => {
+      expect(() => drcGrids.rowsEqual([])).to.throw(TypeError, /Input must be two arrays/);
+      expect(() => drcGrids.rowsEqual([],{})).to.throw(TypeError, /Input must be two arrays/);
+      expect(() => drcGrids.rowsEqual(7,[])).to.throw(TypeError, /Input must be two arrays/);
+      expect(() => drcGrids.rowsEqual([],'hello')).to.throw(TypeError, /Input must be two arrays/);
     });
   });
 });
