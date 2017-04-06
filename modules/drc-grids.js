524569333791f3390b4098185276d10d111915bb
Dale <drcorns@hotmail.com>
2017-04-06 16:07:38 -0700

    add getRowIdxFromRow

--- modules/drc-grids.js
+++ modules/drc-grids.js
@@ -4,19 +4,28 @@
  * Copyright © 2017 Dale Corns
  */
 'use strict';
+const rowsEqual = (r1, r2) =>{
+  if(!(Array.isArray(r1)) || !(Array.isArray(r2))) throw new TypeError('Input must be two arrays');
+  if(!(r1.length === r2.length)) return false;
+  if(r1.length < 100) {
+    return  r1.reduce((out, r) => {
+      return out && (r === r2[r1.indexOf(r)]);
+    }, true);
+  }
+  //Implement code to efficiently handle large arrays by dividing into chunks to test equality without always traversing the entire array.
+  else throw new TypeError('Arrays are too large');
+};
 module.exports = {
   getRowStrings: (row) => {
     if(!(Array.isArray(row))) throw new TypeError('Input must be an array');
     return row.filter(r => typeof r === 'string');
   },
-  rowsEqual: (r1, r2) => {
-    if(!(Array.isArray(r1)) || !(Array.isArray(r2))) throw new TypeError('Input must be two arrays');
-    if(!(r1.length === r2.length)) return false;
-    if(r1.length < 100) {
-      return  r1.reduce((out, r) => {
-        return out && (r === r2[r1.indexOf(r)]);
-      }, true);
-    }
-    else throw new TypeError('Arrays are too large');
-  },
+  rowsEqual: rowsEqual,
+  getRowIdxFromRow: function getRowIdxFromRow(grid, row){
+    //if(!Array.isArray(Array.isArray(grid)) || !Array.isArray(row)) throw new TypeError('Input must be (2dim Array, Array)');
+    return grid.reduce((out, r, idx) => {
+      let x = (rowsEqual(r, row)) ? idx + 1 : 0;
+      return out + x;
+    }, -1);
+  }
 };
--- test/drc-grids.spec.js
+++ test/drc-grids.spec.js
@@ -4,8 +4,8 @@ const expect = require ('chai').expect;
 const drcGrids = require('../modules/drc-grids');
 describe('drc-grids.js', () => {
 
-  describe('getRowStrings', () => {
-    it('Input: Array, Output: Array containing only items with string values from input', () => {
+  describe('getRowStrings(row)', () => {
+    it('Returns an array of the strings contained in row', () => {
       expect(drcGrids.getRowStrings(['f', 108, 'a', 34, 'g', 4, '', 34, 23, 108])).to.deep.equal(['f', 'a', 'g', '']);
     });
     it('It throws a TypeError error for non array input', () => {
@@ -14,11 +14,11 @@ describe('drc-grids.js', () => {
       expect(() => drcGrids.getRowStrings({})).to.throw(TypeError, /Input must be an array/);
       expect(() => drcGrids.getRowStrings('')).to.throw(TypeError, /Input must be an array/);
     });
-    it('Returns an empty array if the array does not contain strings', () => {
+    it('Returns an empty array if the row does not contain strings', () => {
       expect(drcGrids.getRowStrings([108, 34, 4, 34, 23, 108, {}])).to.deep.equal([]);
     });
   });
-  describe('rowsEqual', () => {
+  describe('rowsEqual(row, row)', () => {
     it('Returns true if two arrays are given as input and they are equal', () => {
       expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello'])).to.be.true;
       expect(drcGrids.rowsEqual([], [])).to.be.true;
@@ -28,11 +28,51 @@ describe('drc-grids.js', () => {
       expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello world'])).to.be.false;
       expect(drcGrids.rowsEqual([34, 5, 's', 'hello'], [34, 5, 's', 'hello', 'plus one'])).to.be.false;
     });
-    it('Throws a type error if input is not two arrays', () => {
+    it('Throws a TypeError if input is not two arrays', () => {
       expect(() => drcGrids.rowsEqual([])).to.throw(TypeError, /Input must be two arrays/);
       expect(() => drcGrids.rowsEqual([],{})).to.throw(TypeError, /Input must be two arrays/);
       expect(() => drcGrids.rowsEqual(7,[])).to.throw(TypeError, /Input must be two arrays/);
       expect(() => drcGrids.rowsEqual([],'hello')).to.throw(TypeError, /Input must be two arrays/);
     });
+  });
+  describe('getRowIdxFromRow(grid, row)', () => {
+    it('Returns the Y index of the row if it exists in the grid', () => {
+      expect(drcGrids.getRowIdxFromRow([
+        ['x', 2, 'g', 5, 'r', 9],
+        ['x1', 2, 'g1', 5, 'r1', 9],
+        ['x2', 2, 'g2', 5, 'r2', 9],
+        ['x3', 2, 'g3', 5, 'r3', 9]
+      ],
+        ['x2', 2, 'g2', 5, 'r2', 9])).to.equal(2);
+    });
+    it('Returns -1 if row does not exist in grid', () => {
+      expect(drcGrids.getRowIdxFromRow([
+          ['x', 2, 'g', 5, 'r', 9],
+          ['x1', 2, 'g1', 5, 'r1', 9],
+          ['x2', 2, 'g2', 5, 'r2', 9],
+          ['x3', 2, 'g3', 5, 'r3', 9]
+        ],
+        ['noMatch', 2, 'g2', 5, 'r2', 9])).to.equal(-1);
+      expect(drcGrids.getRowIdxFromRow([
+          ['x', 2, 'g', 5, 'r', 9],
+          ['x1', 2, 'g1', 5, 'r1', 9],
+          ['x2', 2, 'g2', 5, 'r2', 9],
+          ['x3', 2, 'g3', 5, 'r3', 9]
+        ],
+        ['x2', 2, 'g2', 5, 'r2', 9, 'to long'])).to.equal(-1);
+      expect(drcGrids.getRowIdxFromRow([
+          ['x', 2, 'g', 5, 'r', 9],
+          ['x1', 2, 'g1', 5, 'r1', 9],
+          ['x2', 2, 'g2', 5, 'r2', 9],
+          ['x3', 2, 'g3', 5, 'r3', 9]
+        ],
+        ['x2', 2, 'g2', 5, 'r2', 9].reverse())).to.equal(-1);
+    });
+    // it('Throws a type error if parameters are incorrect or missing', () => {
+    //   it('Throws for 0 to 1 arguments passed', () => {
+    //     expect(() => drcGrids.getRowIdxFromRow()).to.throw(TypeError, /Input must be (2dim Array, Array)/);
+    //     expect(() => drcGrids.getRowIdxFromRow([], [])).to.throw(TypeError, /Input must be (2dim Array, Array)/);
+    //   });
+    // });
   });
 });
